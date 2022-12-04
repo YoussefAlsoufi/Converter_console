508c871ad8e9851fc498996a55ab04c7fcfdbbd8
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2022-12-04 22:04:18 +0100

    added files

---
+++ converter/ConverterTools.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+
+namespace Converter
+{
+	public class ConverterTools
+	{
+        public NameValueCollection lengthSection, dataTypeSection;
+		string errorMessage = "";
+        public ConverterTools()
+		{
+            this.lengthSection = (NameValueCollection)ConfigurationManager.GetSection("Length");
+            this.dataTypeSection = (NameValueCollection)ConfigurationManager.GetSection("Data");
+        }
+
+        public string DoConvert(string num, string fromUnit, string toUnit)
+		{
+	
+			double result = 0;
+			string resultMessage;
+			var (checkStatus, usedSection)  = GenericCheckInputs(num, fromUnit, toUnit);
+			if (checkStatus)
+			{
+				double inputValue = Convert.ToDouble(num);
+				double from = Convert.ToDouble(usedSection[Singularize(fromUnit)]);
+				double to = Convert.ToDouble(usedSection[Singularize(toUnit)]);
+
+				result = inputValue * from / to;
+
+	
+				 resultMessage = $"( {num} {fromUnit},  {toUnit}) -> {result.ToString()}";       
+
+            }
+			else
+			{
+				resultMessage = $"Please, Check your Inputs again! you have entered incorrect units or nulls.";
+            }
+
+			return resultMessage;
+		}
+
+        private (bool valid, NameValueCollection Usedsection)  GenericCheckInputs(string inputNum, string fromUnit, string toUnit)
+        {
+			NameValueCollection Usedsection;
+
+            if (this.lengthSection.AllKeys.Contains(fromUnit))
+			{
+                Usedsection = lengthSection;
+			}else
+			{
+                Usedsection = dataTypeSection;
+
+            }
+            bool emptyCheck = ((!String.IsNullOrEmpty(inputNum)) && (!String.IsNullOrEmpty(fromUnit)) && (!String.IsNullOrEmpty(toUnit)));
+            bool validNum = int.TryParse(inputNum, out int n) && n > 0;
+            bool validInput = (Usedsection.AllKeys.Contains(Singularize(fromUnit)) && Usedsection.AllKeys.Contains(Singularize(toUnit)));
+
+            return ((validInput && validNum && emptyCheck), Usedsection);
+        }
+
+  //      private static bool checkcheckInputs(NameValueCollection section, string inputNum, string fromUnit, string toUnit)
+		//{
+
+		//	bool emptyCheck = ((!String.IsNullOrEmpty(inputNum)) && (!String.IsNullOrEmpty(fromUnit)) && (!String.IsNullOrEmpty(toUnit)));
+		//	bool validNum = int.TryParse(inputNum, out int n) && n > 0;
+		//	bool validInput = (section.AllKeys.Contains(Singularize(fromUnit)) && section.AllKeys.Contains(Singularize(toUnit)));
+
+		//	return (validInput && validNum && emptyCheck);
+		//}
+
+        protected static string Singularize(string inputString)
+        {
+			if (!string.IsNullOrEmpty(inputString))
+			{
+                var Singular = System.Data.Entity.Design.PluralizationServices.PluralizationService.CreateService(new System.Globalization.CultureInfo("en-us"));
+                return Singular.Singularize(inputString).ToLower().Trim();
+            }
+            else
+			{
+				return "";
+			}
+
+            
+        }
+
+    }
+
+}
+
---
+++ converter/Tester.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using Converter;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            
+            ConverterTools converter = new ConverterTools();
+
+            //NameValueCollection lengthsSection = converter.lengthSection;
+
+            Console.Write(" km > m \n");
+            Console.Write(converter.DoConvert("2", "kilometer", "meter") + "\n");
+            Console.Write(" m > mm\n");
+            Console.Write(converter.DoConvert("3", "    meter", "millimeter") + "\n");
+            Console.Write("m > km \n");
+            Console.Write(converter.DoConvert("4", "meter", "kilometer") + "\n");
+            Console.Write("mm > inch \n");
+            Console.Write(converter.DoConvert("4", "millimeters", "inch") + "\n");
+            Console.Write("m > inch Plural inputs \n");
+            Console.Write(converter.DoConvert("4", "meters", "inches") + "\n");
+            Console.Write("m > inch NumberInput Is string \n");
+            Console.Write(converter.DoConvert("F", "meter", "inches") + "\n");
+            Console.Write("m > inch fromUnit Is Empty \n");
+            Console.Write(converter.DoConvert("F", "", "inches") + "\n");
+            Console.Write("m > inch fromUnit Is Null \n");
+            Console.Write(converter.DoConvert("F", null, "inches") + "\n");
+            Console.Write("m > inch Negative InputNum \n");
+            Console.Write(converter.DoConvert("-20", "meter", "inches") + "\n");
+            Console.Write("m > inch InputNum=0 \n");
+            Console.Write(converter.DoConvert("4", "meter", "inches") + "\n");
+
+            Console.Write("byte > bit  \n");
+            Console.Write(converter.DoConvert("4", "bytes", "bits") + "\n");
+
+            Console.Write("byte > kilobyte  \n");
+            Console.Write(converter.DoConvert("4", "bytes", "kilobyte") + "\n");
+
+            Console.Write("byte > meter  \n");
+            Console.Write(converter.DoConvert("4", "bytes", "meters") + "\n");
+
+
+            Console.Write("");
+            }
+        }
+    }
