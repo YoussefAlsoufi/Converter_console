971a219ab29150eefbfb66b082914d3418b449c4
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2022-12-05 05:45:02 +0100

    Add A NUnit Test file.

---
+++ converter/ConvertTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Specialized;
+using Converter;
+using NUnit.Framework;
+using Xunit;
+using DemoLibrary;
+namespace converter
+{
+	
+	public class ConverterTests
+	{
+        
+        [Fact]
+		public void TestDoConvert()
+		{
+            NameValueCollection Usedsection = new NameValueCollection();
+            ConverterTools converter = new ConverterTools();
+
+            var negativeCheck = converter.DoConvert("-20", "meter", "kilometer");
+			var nullCheck = ((!String.IsNullOrEmpty("")) && (!String.IsNullOrEmpty("bit")) && (!String.IsNullOrEmpty("gigabyte")));
+            var longthContainsCheck = Usedsection.AllKeys.Contains(ConverterTools.Singularize("Inches")) && Usedsection.AllKeys.Contains(ConverterTools.Singularize("feet"));
+			var validInput= int.TryParse("F", out int n);
+
+
+            Xunit.Assert.Multiple( ()=>
+			{
+				Xunit.Assert.Equal(negativeCheck, (0.2).ToString());
+                Xunit.Assert.True(nullCheck, "Input has not be null or Empty");
+                Xunit.Assert.True(longthContainsCheck, "Both Units should be in the same section {Length, DataType, Tempertures ...}");
+                Xunit.Assert.True(validInput, "The input has to be a number!");
+
+			}
+			
+			);
+					
+
+				
+		}
+	}
+}
+
