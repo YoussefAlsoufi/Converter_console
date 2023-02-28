053c5b2a2cc2bec93cfec943faa4e7f49c74602a
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2023-02-28 22:46:14 +0100

    Avoid NullReferenceException

--- converter/ConverterTools.cs
+++ converter/ConverterTools.cs
@@ -7,102 +7,118 @@ using System.Globalization;
 
 namespace Converter
 {
-	public class ConverterTools
-	{
-        public NameValueCollection lengthSection, dataTypeSection, tempretureSection;
+    public class ConverterTools
+    {
+        public NameValueCollection? lengthSection, dataTypeSection, tempretureSection;
         string resultMessage = "";
+
         public ConverterTools()
-		{
-            this.lengthSection = (NameValueCollection)ConfigurationManager.GetSection("Length");
-            this.dataTypeSection = (NameValueCollection)ConfigurationManager.GetSection("Data");
-            this.tempretureSection = (NameValueCollection)ConfigurationManager.GetSection("Tempreture");
+        {
+            this.lengthSection = (NameValueCollection?)ConfigurationManager.GetSection("Length");
+            this.dataTypeSection = (NameValueCollection?)ConfigurationManager.GetSection("Data");
+            this.tempretureSection = (NameValueCollection?)ConfigurationManager.GetSection("Tempreture");
         }
 
         public string DoConvert(string num, string fromUnit, string toUnit)
-		{
-	
-			double result = 0;
+        {
 
-			var (checkStatus, usedSection)  = GenericCheckInputs(num, fromUnit, toUnit);
-			if (checkStatus)
-			{
-				double inputValue = Convert.ToDouble(num);
-				if (usedSection == tempretureSection)
-				{
-					result= tempConvert(inputValue, Singularize(fromUnit), Singularize(toUnit));
-				}
-				else
-				{
+
+            double result;
+
+            var (checkStatus, usedSection) = GenericCheckInputs(num, fromUnit, toUnit);
+            if (checkStatus)
+            {
+                double inputValue = Convert.ToDouble(num);
+                if (usedSection == tempretureSection)
+                {
+                    result = tempConvert(inputValue, Singularize(fromUnit), Singularize(toUnit));
+                }
+                else
+                {
                     double from = Convert.ToDouble(usedSection[Singularize(fromUnit)]);
                     double to = Convert.ToDouble(usedSection[Singularize(toUnit)]);
 
                     result = inputValue * from / to;
-          
+
                 }
 
-                resultMessage = $"( {num} {fromUnit},  {toUnit}) -> {result.ToString()}";
+                resultMessage = $"( {num} {fromUnit},  {toUnit}) -> {result}";
             }
-			else
-			{
-				resultMessage = $"Please, Check your Inputs again! you have entered incorrect units or nulls.";
-            }
-
-			return resultMessage;
-		}
-
-        private (bool valid, NameValueCollection Usedsection)  GenericCheckInputs(string inputNum, string fromUnit, string toUnit)
-        {
-			NameValueCollection Usedsection = new NameValueCollection();
-			bool wrongInputs = true;
-
-            if (this.lengthSection.AllKeys.Contains(Singularize(fromUnit)))
-			{
-                Usedsection = lengthSection;
-			}
-			else if((this.dataTypeSection.AllKeys.Contains(Singularize(fromUnit))))
-			{
-                Usedsection = dataTypeSection;
-
-            }
-			else if (this.tempretureSection.AllKeys.Contains(Singularize(fromUnit)))
+            else
             {
-				Usedsection = tempretureSection;
+                resultMessage = $"Please, Check your Inputs again! you have entered incorrect units or nulls.";
             }
-			else
-			{
-				 wrongInputs = false;
-			}
-            bool emptyCheck = ((!String.IsNullOrEmpty(inputNum)) && (!String.IsNullOrEmpty(fromUnit)) && (!String.IsNullOrEmpty(toUnit)));
-			bool validNum = int.TryParse(inputNum, out int n);
-            bool validInput = (Usedsection.AllKeys.Contains(Singularize(fromUnit)) && Usedsection.AllKeys.Contains(Singularize(toUnit)));
-			bool positiveValue = true ? ((tempretureSection.AllKeys.Contains(Singularize(fromUnit))) || n > 0) : false;
 
-      
-            return ((validInput && validNum && emptyCheck && wrongInputs && positiveValue), Usedsection);
+            return resultMessage;
         }
 
-		private static double tempConvert(double tempInput, string fromTempUnit, string toTempUnit)
-		{
-			if (fromTempUnit== "celsiu")
-			{
-				var result = (tempInput * 1.8) + 32;
-				return result;
-			}
-			return ((tempInput - 32) / 1.8);
-		}
+        private (bool valid, NameValueCollection Usedsection) GenericCheckInputs(string inputNum, string fromUnit, string toUnit)
+        {
+            NameValueCollection UsedSection = new();
+            bool wrongInputs = true;
+            bool emptyCheck, validNum, validInput, positiveValue;
+
+            if (inputNum is not null && fromUnit is not null && toUnit is not null)
+            {
+                if (lengthSection is not null && lengthSection.AllKeys.Contains(Singularize(fromUnit)))
+                {
+                    UsedSection = lengthSection;
+                }
+                else if (dataTypeSection is not null && dataTypeSection.AllKeys.Contains(Singularize(fromUnit)))
+                {
+                    UsedSection = dataTypeSection;
+
+                }
+                else if (tempretureSection is not null && tempretureSection.AllKeys.Contains(Singularize(fromUnit)))
+                {
+                    UsedSection = tempretureSection;
+                }
+                else
+                {
+                    wrongInputs = false;
+                }
+
+                emptyCheck = (!String.IsNullOrEmpty(inputNum)) && (!String.IsNullOrEmpty(fromUnit)) && (!String.IsNullOrEmpty(toUnit));
+                validNum = int.TryParse(inputNum, out int n);
+                validInput = UsedSection.AllKeys.Contains(Singularize(fromUnit)) && UsedSection.AllKeys.Contains(Singularize(toUnit));
+                positiveValue = true ? (tempretureSection is not null && tempretureSection.AllKeys.Contains(Singularize(fromUnit)) || n > 0) : false;
+
+
+                return (validInput && validNum && emptyCheck && wrongInputs && positiveValue, UsedSection);
+            }
+            else
+            {
+                Console.WriteLine("The Section is not exist, please add a collection in Config file");
+                return (false, UsedSection);
+
+
+            }
+
+        }
+
+        private static double tempConvert(double tempInput, string fromTempUnit, string toTempUnit)
+        {
+            if (fromTempUnit == "celsiu")
+            {
+                var result = (tempInput * 1.8) + 32;
+                return result;
+            }
+            return ((tempInput - 32) / 1.8);
+        }
         public static string Singularize(string inputString)
         {
-			if (!string.IsNullOrEmpty(inputString))
-			{
+            if (!string.IsNullOrEmpty(inputString))
+            {
                 var Singular = System.Data.Entity.Design.PluralizationServices.PluralizationService.CreateService(new System.Globalization.CultureInfo("en-us"));
+                var test = Singular.Singularize(inputString).ToLower().Trim();
                 return Singular.Singularize(inputString).ToLower().Trim();
             }
             else
-			{
-				return "";
-			}
+            {
+                return "";
+            }
 
-            
+
         }
 
     }
